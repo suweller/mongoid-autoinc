4ac39694f6f822f0787e3f0b0a5154a68789c95e
Steven Weller <github@suweller.com>
2013-06-18 12:38:30 +0200

    Use new specstyle

--- spec/autoinc_spec.rb
+++ spec/autoinc_spec.rb
@@ -1,101 +1,75 @@
 require "spec_helper"
 
 describe "Mongoid::Autoinc" do
-
-  after do
-    User.delete_all
-  end
+  after { User.delete_all }
 
   context "class methods" do
-
     subject { User }
 
     it { should respond_to(:increments) }
-
     it { should respond_to :incrementing_fields }
 
     describe "incrementing_fields" do
-
       subject { User.incrementing_fields }
 
       it { should == {:number => {:auto => true}} }
-
       it "should protect number" do
         User.protected_attributes.include? :number
       end
 
       context "for SpecialUser" do
-
         subject { SpecialUser.incrementing_fields }
 
         it { should == {:number => {:auto => true}} }
-
         it "should protect number" do
           User.protected_attributes.include? :number
         end
-
       end
 
       context "for PatientFile" do
-
         subject { PatientFile.incrementing_fields }
 
         it { should == {:file_number => {:scope => :name, :auto => true}} }
-
       end
 
       context "for Operation" do
-
         subject { Operation.incrementing_fields }
 
         it { should == {:op_number => {:scope => subject[:op_number][:scope], :auto => true}} }
         it { subject[:op_number][:scope].should be_a Proc }
-
       end
 
       context "for Vehicle" do
-
         subject { Vehicle.incrementing_fields }
 
         it { should == {:vin => {:seed => 1000, :auto => true}} }
-        
       end
 
       context "for Ticket" do
-
         subject { Ticket.incrementing_fields }
 
         it { should == {:number => {:step => 2, :auto => true}} }
-
       end
 
       context "for LotteryTicket" do
-
         subject { LotteryTicket.incrementing_fields }
 
         it { should == {:number => {:step => subject[:number][:step], :auto => true}} }
         it { subject[:number][:step].should be_a Proc }
-
       end
-
     end
-
   end
 
   context "instance methods" do
-
     let(:incrementor) { Object.new }
-
     before { incrementor.stub!(:inc).and_return(1) }
 
     context "without scope" do
-
       subject { User.new }
 
       it { should respond_to(:update_auto_increments) }
 
       describe "before create" do
-
         let(:user) { User.new(:name => 'Dr. Cox') }
 
         it "should call the autoincrementor" do
@@ -107,7 +81,6 @@ describe "Mongoid::Autoinc" do
         end
 
         describe "writing the attribute" do
-
           before { Mongoid::Autoinc::Incrementor.stub!(:new).and_return(incrementor) }
 
           it "should write the returned incrementor attribute" do
@@ -115,26 +88,19 @@ describe "Mongoid::Autoinc" do
               user.save!
             }.to change(user, :number).from(nil).to(1)
           end
-
         end
 
         describe "#assign!" do
-
           it "should raise AutoIncrementsError" do
             expect { subject.assign!(:number) }.
               to raise_error(Mongoid::Autoinc::AutoIncrementsError)
           end
-
         end
-
       end
-
     end
 
     context "with scope as symbol" do
-
       describe "before create" do
-
         let(:patient_file) { PatientFile.new(:name => 'Dr. Cox') }
 
         it "should call the autoincrementor" do
@@ -144,15 +110,11 @@ describe "Mongoid::Autoinc" do
 
           patient_file.save!
         end
-
       end
-
     end
 
     context "with scope as proc" do
-
       describe "before create" do
-
         let(:user) { User.new(:name => 'Dr. Cox') }
         let(:operation) { Operation.new(:name => 'Heart Transplant', :user => user) }
 
@@ -163,27 +125,21 @@ describe "Mongoid::Autoinc" do
 
           operation.save!
         end
-
       end
-
     end
 
     context "without auto" do
-
       subject { Intern.new }
 
       describe "before create" do
-
         it "should not call the autoincrementor" do
           Mongoid::Autoinc::Incrementor.should_not_receive(:new)
 
           subject.save!
         end
-
       end
 
       describe "#assign!" do
-
         it "should call the autoincrementor" do
           Mongoid::Autoinc::Incrementor.should_receive(:new).
             with('Intern', :number, {auto: false}).and_return(incrementor)
@@ -196,13 +152,10 @@ describe "Mongoid::Autoinc" do
           expect { subject.assign!(:number) }.
             to raise_error(Mongoid::Autoinc::AlreadyAssignedError)
         end
-
       end
 
       context "class with overwritten model name" do
-
         subject { Intern.new }
-
         before { Intern.stub(:model_name => 'PairOfScrubs') }
 
         it "should call the autoincrementor" do
@@ -211,15 +164,11 @@ describe "Mongoid::Autoinc" do
 
           subject.assign!(:number)
         end
-
       end
-
     end
 
     context "with seed" do
-
       describe "before create" do
-
         let(:vehicle) { Vehicle.new(:model => 'Coupe') }
 
         it "should call the autoincrementor with the seed value" do
@@ -229,15 +178,11 @@ describe "Mongoid::Autoinc" do
 
           vehicle.save!
         end
-
       end
-
     end
 
     context "with Integer step" do
-
       describe "before create" do
-
         let(:ticket) { Ticket.new }
 
         it "should call the autoincrementor with the options hash" do
@@ -247,15 +192,11 @@ describe "Mongoid::Autoinc" do
 
           ticket.save!
         end
-
       end
-
     end
 
     context "with Proc step" do
-
       describe "before create" do
-
         let(:lottery_ticket) { LotteryTicket.new(start: 10) }
 
         it "should call the autoincrementor with the options hash" do
@@ -265,11 +206,7 @@ describe "Mongoid::Autoinc" do
 
           lottery_ticket.save!
         end
-
       end
-
     end
-
   end
-
 end
--- spec/incrementor_spec.rb
+++ spec/incrementor_spec.rb
@@ -1,84 +1,63 @@
 require "spec_helper"
 
 describe "Mongoid::Autoinc::Incrementor" do
-
   let(:options) { {scope: '123', seed: 100, step: 2} }
   let(:incrementor) { Mongoid::Autoinc::Incrementor.new('User', :number, options) }
 
   describe "model name" do
-
     subject { incrementor.model_name }
 
     it { should == 'User' }
-
   end
 
   describe "field name" do
-
     subject { incrementor.field_name }
 
     it { should == 'number' }
-
   end
 
   describe "scope key" do
-
     subject { incrementor.scope_key }
 
     it { should == '123' }
-
   end
 
   describe "seed" do
-
     subject { incrementor.seed }
 
     it { should == 100 }
-
   end
 
   describe "step" do
-
     subject { incrementor.step }
 
     it { should == 2 }
-
   end
 
   describe "#key" do
-
     subject { incrementor.key }
 
     it { should == 'user_number_123' }
 
     context "without scope" do
-
       subject { Mongoid::Autoinc::Incrementor.new('User', :number).key }
 
       it { should == 'user_number'}
-
     end
 
     context "for a subclass" do
-
       let(:incrementor) do
-
         Mongoid::Autoinc::Incrementor.new('SpecialUser', :number, options)
-
       end
 
       it { should == 'special_user_number_123' }
-
     end
-
   end
 
   describe "#inc" do
-
     subject { incrementor.inc }
 
     describe "generating incrementing numbers" do
-
       before { User.delete_all }
 
       it "should increment the number for each document" do
@@ -86,11 +65,9 @@ describe "Mongoid::Autoinc::Incrementor" do
           User.create!(:name => 'Bob Kelso').number.should == incrementing_number
         end
       end
-
     end
 
     context "with a seed value" do
-
       before { Vehicle.delete_all }
 
       it "should start the incrementor at the seed value" do
@@ -98,11 +75,9 @@ describe "Mongoid::Autoinc::Incrementor" do
           Vehicle.create(model: "Coupe").vin.should == 1000 + i
         end
       end
-
     end
 
     context "with a step Integer value" do
-
       before { Ticket.delete_all }
 
       it "should increment according to the step value" do
@@ -110,11 +85,9 @@ describe "Mongoid::Autoinc::Incrementor" do
           Ticket.create.number.should == 2 * i
         end
       end
-
     end
 
     context "with a step Proc value" do
-
       before { LotteryTicket.delete_all }
 
       it "should increment according to the step value" do
@@ -122,9 +95,6 @@ describe "Mongoid::Autoinc::Incrementor" do
         LotteryTicket.create(start: 30).number.should == 42
         LotteryTicket.create.number.should == 43
       end
-
     end
-
   end
-
 end
